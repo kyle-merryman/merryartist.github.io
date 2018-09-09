671c6c8a71fd4d23243f651cb2d96eb89001e686
wintermute <kajmerryman@gmail.com>
2018-09-08 22:45:06 -0700

    pentultimate commit

---
+++ Hangman/hangman.js
@@ -0,0 +1,80 @@
+var wordList = ['tyrannosaurs', 'velociraptor', 'triceratops', 'brachiosaurus', 'stegosaurus', 'allosaurus', "funny", "jawbreaker", "jackpot", "witchcraft", "fishhook", "cobweb", "haiku", "zodiac", "xylophone", "vodka", "puppy"]     // array of words
+var wins = 0;
+var losses = 0;
+var ww = "";
+var underscoreContainer = [];
+var guessCount = 10;
+var lettersGuessed = [];
+var arrayOfWinningWord;
+// random a word        
+ww = wordList[Math.floor(Math.random() * wordList.length)];
+console.log(ww);
+// split picked word into letter array
+var arrayOfWinningWord = ww.split('');
+console.log(arrayOfWinningWord);
+
+//create a for loop which pushes each letter out on the document as an "_"
+function printUnderscore() {
+    for (i = 0; i < arrayOfWinningWord.length; i++) {
+        underscoreContainer.push("_");
+    } 
+    // document.getElementById("ww").append(printUnderscore());
+    document.getElementById
+    console.log(underscoreContainer);            
+}
+//lettersGuessed.contains(userGuess)
+//if (underscoreContainer.join("") == arrayOfWinningWord.join(""));
+//else if (underscoreContainer.join("") == arrayOfWinningWord.join("")) {
+    //console.log("You win!");
+
+//onkeyEvent
+document.onkeyup = function (event) {
+    var userGuess = event.key.toLowerCase();
+    if (lettersGuessed.indexOf(userGuess) > -1 || ) {
+        event.stopPropogation();
+    } else {
+        guessCount--;
+        lettersGuessed.push(userGuess);
+        console.log(userGuess);
+        console.log(lettersGuessed);
+    } 
+    if (underscoreContainer.join("") == arrayOfWinningWord.join("")) {
+        console.log("You win!");
+    }
+    // If letter is not in array, add it to list of absent letters
+
+
+    // if (!ww.contains(userGuess)) {
+    //     lettersGuessed.append(userGuess);
+    //     console.log(lettersGuessed);
+    // }
+
+    //fill the correct answer into right underscore position
+    if (arrayOfWinningWord.includes(userGuess)) {
+        for (j = 0; j < arrayOfWinningWord.length; j++) {
+            if (arrayOfWinningWord[j] == userGuess) {
+                underscoreContainer[j] = userGuess.toUpperCase();
+            }
+        }
+        console.log(underscoreContainer);
+
+
+    } else { 
+        lettersGuessed.append(userGuess);
+        console.log(lettersGuessed);
+    }
+    
+    //Check winning condition
+    //if(underscoreContainer.indexOf("_") === -1){wins ++; console.log("You Win!")}    
+
+
+//
+
+//user guess has to be a letter
+        
+
+
+printUnderscore();        
+
+//if (userGuess = lettersGuess)
+}
